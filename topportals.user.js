e239ce5bb1a53bea08d9cbe65cdcc445fd3809e1
Joakim Östlund <joakim.xo.ostlund@ericsson.com>
2013-02-28 10:29:15 +0100

    Now possible to filter on team

--- topportals.user.js
+++ topportals.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             iitc-plugin-topportals@Zilla
 // @name           iitc: TopPortals
-// @version        0.1
+// @version        0.2
 // @namespace      https://github.com/breunigs/ingress-intel-total-conversion
 // @updateURL      https://raw.github.com/Zilla/iitc-plugin-topportals/master/topportals.user.js
 // @downloadURL    https://raw.github.com/Zilla/iitc-plugin-topportals/master/topportals.user.js
@@ -21,17 +21,26 @@ function wrapper()
     window.plugin.topPortals = function() {};
     
     window.plugin.topPortals.setup = function() {
-	// add a new div to the bottom of the sidebar and style it
+	// Add topportal area to sidebar
 	$('#sidebar').append('<div id="portaltoplist" style="min-height: 240px; color: #ffce00; font-size: 13px; padding: 4px 2px;"></div>');
+
+	// Add settings area to sidebar
+	$('#sidebar').append('<div id="toplistsettings" style="min-height: 20px; color: #ffce00; font-size: 13px; padding: 4px 2px;"></div>');
+	setupSettingsArea();
 	
 	window.addHook('portalAdded', window.plugin.topPortals.onPortalAdded);
+
+	showTeams[TEAM_ENL] = true;
+	showTeams[TEAM_RES] = true;
     }
 
+    // Called at start of script
     var setup = function() {
 	window.plugin.topPortals.setup();
     }
 
     var topportals = [];
+    var showTeams = [];
 
     function addPortalForTopList(portal)
     {
@@ -53,21 +62,27 @@ function wrapper()
 	};
     }
     
-    function drawPortalTopList()
+    window.plugin.topPortals.drawPortalTopList = function()
     {
 	var htstr = "<p>Portal kill AP:</p><div style=\"display: table;\">";
 	var sortarr = [];
 	var idx = 0;
 	for(i in topportals)
 	{
-	    sortarr[idx++] = topportals[i];
+	    if( showTeams[topportals[i].team] == true )
+		sortarr[idx++] = topportals[i];
 	}
 	sortarr.sort(function(a,b) {
 	    if( a.score < b.score ) return 1;
 	    if( a.score > b.score ) return -1;
 	    return 0; });
 	for( i = 0; i < sortarr.length && i < 10; i++ )
-	    htstr += "<a style=\"display: table-row;\" " + sortarr[i].link + "\"><span style=\"display: table-cell;\">" + sortarr[i].score + ":</span><span style=\"display: table-cell; padding: 2px;\"></span><span class=\"" + (sortarr[i].team == TEAM_ENL ? "enl" : "res")  + "\" style=\"display: table-cell;\">" + sortarr[i].name + "</span></a>";
+	{
+	    htstr += "<a style=\"display: table-row;\" " + sortarr[i].link + "\">";
+	    htstr += "<span style=\"display: table-cell;\">" + sortarr[i].score + ":</span>";
+	    htstr += "<span style=\"display: table-cell; padding: 2px;\"></span>";
+	    htstr += "<span class=\"" + (sortarr[i].team == TEAM_ENL ? "enl" : "res")  + "\" style=\"display: table-cell;\">" + sortarr[i].name + "</span></a>";
+	}
 	htstr += "</div>";
 	$('#portaltoplist').html(htstr);
     }
@@ -75,7 +90,30 @@ function wrapper()
     window.plugin.topPortals.onPortalAdded = function(obj) {
 	addPortalForTopList(obj.portal.options);
 	
-	drawPortalTopList();
+	 window.plugin.topPortals.drawPortalTopList();
+    }
+
+    function setupSettingsArea()
+    {
+
+	var htstr = "";
+	htstr += "<input type='checkbox' style='height: 13px; vertical-align: middle' checked onChange='window.plugin.topPortals.toggleRes(this);'> <span class=\"res\">Resistance </span>";
+	htstr += "<input type='checkbox' style='height: 13px; vertical-align: middle' checked onChange='window.plugin.topPortals.toggleEnl(this);'> <span class=\"enl\">Enlightened </span>";
+//	htstr += "<p><input type='radio' name='aptype' style='height: 13px; vertical-align: middle; background: transparent;' checked> Total AP ";
+//	htstr += "<input type='radio' name='aptype' style='height: 13px; vertical-align: middle; color: #ffce00;'> Kill AP</p>";
+	$('#toplistsettings').html(htstr);
+    }
+
+    window.plugin.topPortals.toggleRes = function(cb)
+    {
+	showTeams[TEAM_RES] = cb.checked;
+	window.plugin.topPortals.drawPortalTopList();
+    }
+
+    window.plugin.topPortals.toggleEnl = function(cb)
+    {
+	showTeams[TEAM_ENL] = cb.checked;
+	window.plugin.topPortals.drawPortalTopList();
     }
         
     if(window.iitcLoaded && typeof setup === 'function') {
